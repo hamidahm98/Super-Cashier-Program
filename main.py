ac27475d6b667b0c4583acefa0b62c56c6509321
Hamidah <hamidahmimi98@gmail.com>
2023-02-08 23:49:46 +0700

    Main Super Cashier Program

---
+++ main.py
@@ -0,0 +1,8 @@
+#import cashier module and Transaction class in cashier module
+from cashier import Transaction
+
+#call Transaction class
+trx = Transaction()
+#call input_order and action_menu instances to perform the program
+trx.input_order()
+trx.action_menu()
